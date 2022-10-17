49335fc8b92ddea4f93f1cbccd643076a3e8ec08
Faraz-mobin17 <faraz.mobin17@gmail.com>
2022-10-17 06:15:15 +0530

    updated bubble sort code

--- Program_lab_java/Sorting/BubbleSort/Main.java
+++ Program_lab_java/Sorting/BubbleSort/Main.java
@@ -1,17 +1,16 @@
+package Sorting.BubbleSort;
+
 public class Main {
 
-    public static void swap(int x, int y) {
-        int temp = x;
-        x = y;
-        y = temp;
-    }
-
     public static void bubbleSort(int[] arr, int n) {
+
         for (int i = 0; i < n - 1; i += 1) {
             boolean flag = false;
             for (int j = 0; j < n - 1 - i; j += 1) {
                 if (arr[j] > arr[j + 1]) {
-                    swap(arr[j], arr[j + 1]);
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                     flag = true;
                 }
             }
@@ -26,5 +25,8 @@ public class Main {
         int[] arr = { 8, 5, 3, 7, 2 };
         int n = arr.length;
         bubbleSort(arr, n);
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
 }
