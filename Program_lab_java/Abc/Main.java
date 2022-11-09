4690ec091b0961b0ab2a50b5a2b7396bf27c81ab
Faraz-mobin17 <faraz.mobin17@gmail.com>
2022-11-09 12:16:20 +0530

    added exception program

---
+++ Program_lab_java/Abc/Main.java
@@ -0,0 +1,10 @@
+package Abc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+        System.out.println(n1 / n2);
+    }
+}
