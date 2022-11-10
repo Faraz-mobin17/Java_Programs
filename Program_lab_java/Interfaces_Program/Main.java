7cbe3d8183c64d64a19b0fe406e50689420e64d6
Faraz-mobin17 <faraz.mobin17@gmail.com>
2022-11-10 11:07:06 +0530

    added interface program

---
+++ Program_lab_java/Interfaces_Program/Main.java
@@ -0,0 +1,27 @@
+package Interfaces_Program;
+
+public class Main {
+    public static void main(String[] args) {
+        C first = new C();
+        first.display();
+        first.show();
+    }
+}
+
+interface A {
+    void display();
+}
+
+interface B {
+    void show();
+}
+
+class C implements A, B {
+    public void display() {
+        System.out.println("display method0");
+    }
+
+    public void show() {
+        System.out.println("show method");
+    }
+}
