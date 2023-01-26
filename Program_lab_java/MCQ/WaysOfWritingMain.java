7a33799fddb97266cc1bedc8ea76af0cb1cb3bb8
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-01-26 23:49:25 +0530

    ways of Writing main method

---
+++ Program_lab_java/MCQ/WaysOfWritingMain.java
@@ -0,0 +1,19 @@
+package MCQ;
+
+public class WaysOfWritingMain {
+    static public void main(String... args) {
+        System.out.println("Hello world");
+    }
+    static public void main(String[] args) {
+        System.out.println("hello world");
+    }
+    static public void main(String []args) {
+        System.out.println("hello world");
+    }
+    public static void main(String args[]) {
+        System.out.println("WaysOfWritingMain.main()");
+    }
+    public static void main(String... args) {
+        System.out.println("WaysOfWritingMain.main()");
+    }
+}
