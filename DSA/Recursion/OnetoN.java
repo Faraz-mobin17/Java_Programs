80e8c2954383d739240013e4b00d37db0b60e7b6
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-01-26 23:38:51 +0530

    added recursion program to print 1 to N numbers

---
+++ DSA/Recursion/OnetoN.java
@@ -0,0 +1,14 @@
+package DSA.Recursion;
+
+public class OnetoN {
+    public static void main(String[] args) {
+        fun(5);
+    }
+    static void fun(int n) {
+        if (n == 0) {
+            return;
+        }
+        fun(n - 1);
+        System.out.println(n);
+    }
+}
