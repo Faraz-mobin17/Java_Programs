b139bd0081cd19e2a1d38f10226e7f1d646fd9d5
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-01-29 18:19:56 +0530

    added valid and invalid syntaxes

--- Foundation/ArrayProgram.java
+++ Foundation/ArrayProgram.java
@@ -21,5 +21,17 @@ public class ArrayProgram {
         int[][] []a;
         int [][]a[];
         int []a[][];
+        // valid array syntax
+        int[] z = new int[0];
+        int[] d = new int['a'];
+        byte bb = 20;
+        int[] e = new int[bb];
+        short s = 30;
+        int[] ss = new int[s];
+        int[] range = new int[2147483647]; // how many bytes will be allocated = 2147483647 * 4 bytes if this much memory is not available in our system then we will get out of memory error
+        // invalid array syntax
+        int[] x = new int[-3]; // runtime exception negative array size exception
+        int[] l = new int[10l]; // invalid syntax only byte, short , char and int are allowed if any other type then we will get compile time error 
+        int[] range2 = new int[2147483648]; // compile time error integer number too large
     }
 }
