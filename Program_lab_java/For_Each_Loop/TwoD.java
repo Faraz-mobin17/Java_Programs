20a6318234a2fb31db9033a536b72c7b60beebe3
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-02-06 23:44:14 +0530

    added 2d array program in foreach loop folder

---
+++ Program_lab_java/For_Each_Loop/TwoD.java
@@ -0,0 +1,23 @@
+package For_Each_Loop;
+
+public class TwoD {
+    public static void main(String[] args) {
+        int[] nums = {5,2,8,1};
+        int[][] nums2 = {
+            {5,2,1,3},
+            {1,2,3,4},
+            {3,2,1,4,4}
+        };
+        for (int x : nums) {
+            System.out.println(x);
+        }
+        System.out.println();
+        System.out.println("2d array");
+        for (int x[] : nums2){ 
+            for (int b : x) {
+                System.out.print(b + " ");
+            }
+            System.out.println();
+        }
+    }
+}
