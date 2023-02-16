cad6148068059352c7c45b3121c4222984211db1
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-02-16 17:07:03 +0530

    check kth bit program

--- Program_lab_java/Bitwise/checkKthBit.java
+++ Program_lab_java/Bitwise/checkKthBit.java
@@ -1,32 +1,21 @@
 package Bitwise;
+import java.util.Scanner;
 
 public class checkKthBit {
-    static boolean checkKbit2(int n, int k) {
-        int x = (1 << (k - 1));
-        if ((n & x) != 0)
-            return true;
-        else
-            return false;
-    }
-
-    static boolean checkKbit(int n, int k) {
-        int x = 1;
-        for (int i = 0; i < k - 1; i += 1) {
-            x = x * 2;
-        }
-        if ((n & x) != 0)
-            return true;
-        else
-            return false;
-    }
-
     public static void main(String[] args) {
-        boolean ans = checkKbit2(4, 2);
-        if (ans) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the Kth bit");
+        int k = sc.nextInt();
 
-    }
+         int mask = 1<<(k-1);
+        if((n&mask) != 0){
+            System.out.println("set");
+
+        }else{
+            System.out.println("not set");
+        }
+       
+     }
 }
