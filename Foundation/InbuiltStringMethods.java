f7d634fa34222e3c5b23021b88dba8686c9a7b80
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-02-24 22:40:05 +0530

    inbuilt string method

---
+++ Foundation/InbuiltStringMethods.java
@@ -0,0 +1,18 @@
+package Foundation;
+
+public class InbuiltStringMethods 
+{
+    public static void main(String[] args) 
+    {
+        String str = "PW Skills Java";
+        System.out.println("String: " + str);
+        System.out.println("To Uppercase: " + str.toUpperCase());        
+        System.out.println("To Lowercase: " + str.toLowerCase());
+        System.out.println("Character At: " + str.charAt(0));
+        System.out.println("String Length: " + str.length());
+        System.out.println("Substring: " + str.substring(1));
+        System.out.println("Substring2: " + str.substring(1,4)); // beginIndex is inclusive and is exclusive
+        System.out.println("Index of: " + str.indexOf('a'));
+        System.out.println("Last Index of: " + str.lastIndexOf('a'));
+    }
+}
