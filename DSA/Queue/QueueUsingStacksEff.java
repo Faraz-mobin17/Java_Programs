5a44565b232ffe43ac03c0860a069064aeedec0b
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-07 07:29:16 +0530

    Queue using Two Stack efficient program

---
+++ DSA/Queue/QueueUsingStacksEff.java
@@ -0,0 +1,40 @@
+package DSA.Queue;
+import java.util.Stack;
+public class QueueUsingStacksEff {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+    }
+}
+
+class QuisngStackEff 
+{
+    private Stack<Integer> first,second;
+    public QuisngStackEff() {
+        first = second = new Stack<>();
+    }
+    public void add(int item) throws Exception
+    {
+        while (!first.isEmpty())
+        {
+            second.push(first.pop());
+        }
+        first.push(item);
+        while (!second.isEmpty())
+        {
+            first.push(second.pop());
+        }
+    }
+
+    public int peek() throws Exception
+    {
+        return first.peek();
+    }
+    public int remove() throws Exception
+    {
+        return first.pop();
+    }
+    public boolean isEmpty()
+    {
+        return first.isEmpty();
+    }
+}
