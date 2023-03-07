2f460083c3dddec363f642bcf7a075ede0e14918
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-07 18:37:06 +0530

    Encapsulation program added in encapsulation

---
+++ Foundation/Encapsulation/Student.java
@@ -0,0 +1,41 @@
+package Foundation.Encapsulation;
+public class Student {
+    private int age;
+    private String name;
+    public void setData()
+    {
+        age = 18;
+        name = "faraz";
+    }
+    public void setData2(int age,String name) {
+        this.age = age;
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {return age;}
+    public String getName() {return name;}
+    public void show() {
+        System.out.println("Age: " + age);
+        System.out.println("Name: " + name);
+    }
+    public static void main(String[] args) {
+        Student obj = new Student();
+        // System.out.println(obj);
+        // System.out.println(obj.age + " " + obj.name);
+        // obj.age = 18;
+        // obj.name = "faraz";
+        // System.out.println(obj.age + " " + obj.name);
+        // now declare both the varible private
+        obj.setData();
+        System.out.println(obj.getAge());
+        System.out.println(obj.getName());
+        obj.setData2(23, "java");
+        System.out.println(obj.getAge());
+        System.out.println(obj.getName());
+    }
+}
