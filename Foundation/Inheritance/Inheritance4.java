d4c784c8c9c2a369c0cbafbe91daa7bd49a41299
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-08 11:45:51 +0530

    added inheritance 4 program

---
+++ Foundation/Inheritance/Inheritance4.java
@@ -0,0 +1,23 @@
+package Foundation.Inheritance;
+// multiple inheritance
+class Demo1 {
+    void display() {
+        System.out.println("display method written in demo1 class");
+    }
+}
+
+class Demo2 extends Demo1 {
+    
+}
+
+class Demo3 extends Demo2 {
+
+
+}
+
+public class Inheritance4 {
+    public static void main(String[] args) {
+        Demo3 d = new Demo3();
+        d.display();        
+    }
+}
