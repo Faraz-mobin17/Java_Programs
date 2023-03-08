d308ab2e944a5b87cb7889aeb62a46aa2d84bb53
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-08 11:44:59 +0530

    added student constructor and moved main method to up

--- Foundation/Inheritance/Inheritance2.java
+++ Foundation/Inheritance/Inheritance2.java
@@ -1,15 +1,32 @@
 package Foundation.Inheritance;
 
+public class Inheritance2 
+{
+    public static void main(String[] args) 
+    {
+        Student s1 = new Student();
+        s1.display();
+    }
+}
+
 class Human 
 {
     int age;
     private String name = "faraz";
     protected int roll_no;
-    Human() {
+    Human() 
+    {
         System.out.println("Human class Constructor");
         System.out.println(name);
     }
-    void sleep() {
+    Human(int age, String name)
+    {
+        System.out.println("Parameterized Constructor");
+        this.age = age;
+        this.name = name;
+    }
+    void sleep() 
+    {
         age = 23;
         System.out.println("Human needs good sleep");
         System.out.println(age);
@@ -18,16 +35,18 @@ class Human
 
 class Student extends Human 
 {
+    public Student() 
+    {
+        // super(5,"garima");
+        System.out.println("Student Constructor Called");
+    }
     void display() 
     {
         System.out.println("The age is: " + age);
         System.out.println("Roll no is: " + roll_no);
-        // System.out.println("The name is: " + name); //will give error because name is private cannot voilate encapsulation principle
+        // System.out.println("The name is: " + name); //will give error because name is 
+        // private cannot voilate encapsulation principle
     }
 }
 
-public class Inheritance2 {
-    public static void main(String[] args) {
-        
-    }
-}
+
