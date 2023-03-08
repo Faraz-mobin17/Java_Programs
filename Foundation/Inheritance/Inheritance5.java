04e15ff796fbeccc0e6bbce4f6b99cdc914728f0
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-08 11:46:13 +0530

    added inheritance 6 programs

---
+++ Foundation/Inheritance/Inheritance5.java
@@ -0,0 +1,23 @@
+package Foundation.Inheritance;
+// hierarchical inheritance
+class Animal {
+    void sleep() {
+        System.out.println("Animal needs to sleep");
+    }
+}
+
+class Tiger extends Animal {
+
+}
+
+class Monkey extends Animal {
+
+}
+
+
+
+public class Inheritance5 {
+    public static void main(String[] args) {
+        
+    }
+}
