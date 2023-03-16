970718254262759231588e7032e62cab74c13866
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-16 19:49:26 +0530

    ✅ Arr initialization part 2

---
+++ Foundation/Arrays/ArrayInitTwo.java
@@ -0,0 +1,15 @@
+package Foundation.Arrays;
+
+public class ArrayInitTwo {
+    public static void main(String[] args) {
+        int[] x = new int[5];
+        x[0] = 1;
+        x[1] = 2;
+        x[2] = 3;
+        x[3] = 4;
+        x[4] = 5;
+        x[2.5] = 6;// compile time error possible loss of pricision double value found
+        x[-5] = 7; // run time exception arrayindexoutofbound
+        x[5] = 8; // run time exception array index out bound
+    }
+}
