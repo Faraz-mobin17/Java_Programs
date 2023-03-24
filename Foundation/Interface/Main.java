a2928cced2333a510eb374e54777bedb14b2829f
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-24 19:32:22 +0530

    ✅Interface explained

--- Foundation/Interface/Main.java
+++ Foundation/Interface/Main.java
@@ -1,9 +1,16 @@
 package Foundation.Interface;
-
+// in an interface variable are public static final 
 interface Computer 
 {
     // public void compileCode(){} if no body declare abstract
-    void compileCode();
+    void compileCode(); // by default it is public abstract
+    void config(); 
+    /*
+     * above we can we that we have config() method now let suppose 
+     * if your code is being used by another company and they added extra methods  in interface after
+     * adding methods you need to update all the classes which are implementing it so this is a issue in backward
+     * compatibility so java has introdcuted new feature in java 8 to tackle this check JavaFe.java file
+     */
 }
 
 class Laptop implements Computer
