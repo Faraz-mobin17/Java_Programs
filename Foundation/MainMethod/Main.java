dc94c1315ca7b37473f245b2e0cb39c1f5bc804f
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-24 19:24:53 +0530

    ✅ Inheritance and main method

---
+++ Foundation/MainMethod/Main.java
@@ -0,0 +1,10 @@
+package Foundation.MainMethod;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("parent main method");
+    }
+}
+
+class child extends Main {
+    
+}
