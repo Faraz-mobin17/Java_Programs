25b70959735eddd0e81752ba9a581085889507d0
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-04-07 17:51:48 +0530

    ✅ Power of command line args

---
+++ Foundation/TypesOfVar/PowerCmdArgs.java
@@ -0,0 +1,7 @@
+public class PowerCmdArgs {
+	public static void main(int[] args) {
+//		int n = Integer.parseInt(args[0]);
+//		System.out.print("Power of : " + args[0] + " is: " + n * n);
+		System.out.println("Hello ");
+	}
+}
