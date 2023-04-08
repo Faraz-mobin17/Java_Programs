5bc655c2b596ae49b158cf019056d955c282e1be
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-04-08 13:55:45 +0530

    ✅ Static method overloading and method hiding

---
+++ Foundation/Static/Parent.java
@@ -0,0 +1,24 @@
+package Foundation.Static;
+
+// compile with javac Parent.java and javacChild.java then run both the file 
+// ouput will be parent main method because that method will be inherited
+// inheritance concepts applicable for static methods and main methods
+/*
+ * Inheritance and method overloaing applicable for static method but instead
+ * of overidding method hiding is applicable 
+ */
+public class Parent {
+    public static void main(String[] args) {
+        System.out.println("parent main method");
+    }
+}
+
+// class Child extends Parent { // ouput parent main method
+// }
+// this concepts in know as method hiding not overriding here
+class Child extends Parent {
+    public static void main(String[] args) {
+
+        System.out.println("child main emthod");
+    }
+}
