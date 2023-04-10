5ab05141727258798f81e05160c51d510ea1e621
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-04-10 18:15:16 +0530

    main method inheritance

--- Foundation/Static/Parent.java
+++ Foundation/Static/Parent.java
@@ -1,14 +1,15 @@
 package Foundation.Static;
 
-// compile with javac Parent.java and javacChild.java then run both the file 
+// compile with javac Parent.java and javacChild.java then run both the file
 // ouput will be parent main method because that method will be inherited
 // inheritance concepts applicable for static methods and main methods
 /*
- * Inheritance and method overloaing applicable for static method but instead
- * of overidding method hiding is applicable 
+ * Inheritance and method overloaing applicable for static method but instead of
+ * overidding method hiding is applicable
  */
 public class Parent {
-    public static void main(String[] args) {
+
+    public static void main(final String[] args) {
         System.out.println("parent main method");
     }
 }
@@ -17,7 +18,7 @@ public class Parent {
 // }
 // this concepts in know as method hiding not overriding here
 class Child extends Parent {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         System.out.println("child main emthod");
     }
