726e824d388839b81ca69f38089926bb658d5ef7
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-05-05 19:43:42 +0530

    ✅ Data hiding OOPS concept

---
+++ Java_codes/src/Foundation/OOPS/DataHiding/Main.java
@@ -0,0 +1,14 @@
+package Foundation.OOPS.DataHiding;
+
+public class Main {
+    // recommended access modifier for data memeber
+    private double balance; // declaring private data memeber is an example of data hiding
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
